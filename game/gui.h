7f5c80a516436dce0f21a08c1d80d232f7f75b6e
redplanetcat <mettbs@gmail.com>
2026-07-17 15:42:48 +1100

    Work on ui plotting

--- game/gui.h
+++ game/gui.h
@@ -18,7 +18,7 @@ enum class layout: u32 {
 
 enum class container_flags: u32 {
   Hidden,
-  FitContent,
+  Expand,
 };
 
 enum class margin: u32 {
@@ -28,12 +28,27 @@ enum class margin: u32 {
   Left
 };
 
+enum class align: u8 {
+  Top,
+  Center,
+  Bottom
+};
+
+enum class justify: u8 {
+  Left,
+  Center,
+  Right
+};
+
 struct style {
   color BaseColor;
   color HotColor;
   color ActiveColor;
   color TextColor;
+  color ShadowColor;
   f32 ButtonMargin[4];
+  f32 HorizontalGap;
+  f32 VerticalGap;
 };
 
 global const style DefaultStyle = {
@@ -41,16 +56,23 @@ global const style DefaultStyle = {
   {100, 200, 100, 255}, // HotColor
   {200, 100, 100, 255}, // ActiveColor
   {255, 255, 255, 255}, // TextColor
-  {4.f, 8.f, 4.f, 8.f} // ButtonMargin
+  { 50,  50,  50, 255}, // ShadowColor
+  {4.f, 8.f, 4.f, 8.f}, // ButtonMargin
+  8.f, // Horizontal Gap
+  16.f, // Vertical Gap
 };
 
 struct container {
   rect Rect;
   f32 Margin[4];
+  f32 Content[2];
+  f32 Gap;
   u32 Z;
   u32 Flags;
   style Style;
   layout Layout;
+  align Align;
+  justify Justify;
 };
 
 struct context {
@@ -149,26 +171,49 @@ GuiPushContainer(context* CTX, container* Container) {
   CTX->NumContainers++;
 }
 
-internal void
+internal container*
 GuiPopContainer(context* CTX) {
   Assert(CTX->NumContainers > 1);
-  CTX->NumContainers--;
+  container* Container = &CTX->Containers[CTX->NumContainers - 1];
+  CTX->NumContainers -= 1;
+  return (Container);
 }
 
 internal container
-CreateChildContainer(context* CTX, container* Parent) {
+CreateChildContainer(context* CTX, 
+                     container* Parent, 
+                     f32 Width = 0.f,
+                     f32 Height = 0.f,
+                     layout Layout = layout::Horizontal) 
+{
   container Container = {};
-  Container.Rect = {
-    Parent->Rect.x + Parent->Margin[(u32)margin::Left],
-    Parent->Rect.y + Parent->Margin[(u32)margin::Top],
-    Parent->Rect.w - 
-      (Parent->Margin[(u32)margin::Right] + Parent->Margin[(u32)margin::Left]),
-    Parent->Rect.h - 
-      (Parent->Margin[(u32)margin::Top] + Parent->Margin[(u32)margin::Bottom])
-  };
-  memset(Container.Margin, 0, sizeof(Container.Margin));
+  f32 LeftMargin = Parent->Margin[(u32)margin::Left];
+  f32 TopMargin = Parent->Margin[(u32)margin::Top];
+  f32 RightMargin = Parent->Margin[(u32)margin::Right];
+  f32 BottomMargin = Parent->Margin[(u32)margin::Bottom];
+  f32 LeftContent = Parent->Content[(u32)layout::Horizontal];
+  f32 TopContent = Parent->Content[(u32)layout::Vertical];
+  f32 LeftGap = (LeftContent > 0.f ? Parent->Gap : 0.f);
+  f32 TopGap = (TopContent > 0.f ? Parent->Gap : 0.f);
+  Container.Rect.x = Parent->Rect.x + LeftMargin + (Parent->Layout == layout::Horizontal
+        ? LeftContent + LeftGap
+        : 0.f);
+  Container.Rect.y = Parent->Rect.y + TopMargin + (Parent->Layout == layout::Vertical
+        ? TopContent + TopGap
+        : 0.f);
+  Container.Rect.w = Width;
+  Container.Rect.h = Height;
+  Parent->Content[(u32)layout::Horizontal] = (Parent->Layout == layout::Horizontal
+        ? LeftContent + LeftGap + Container.Rect.w
+        : Container.Rect.w);
+  Parent->Content[(u32)layout::Vertical] = (Parent->Layout == layout::Vertical
+        ? TopContent + TopGap + Container.Rect.h
+        : Container.Rect.h);
   Container.Z = Parent->Z + 1;
   Container.Flags = Parent->Flags;
+  Container.Gap = (Layout == layout::Horizontal 
+                   ? CTX->Style.HorizontalGap
+                   : CTX->Style.VerticalGap);
   return (Container);
 }
 
@@ -186,6 +231,7 @@ GuiDrawPanel(context* CTX, container* Container) {
   //  Command.Flags |= (u32)draw_command_flags::Flip;
   //}
   Command.Color = Container->Style.BaseColor;
+  Command.Color.a = 150;
   Command.DstRect = Container->Rect;
   Command.Z = Container->Z;
   PushDrawCommand(CTX->CommandStack, &Command);
@@ -234,30 +280,53 @@ PanelBegin(context* CTX, const char* Name,
   Container.Style = CTX->Style;
   Container.Layout = Layout;
   GuiPushContainer(CTX, &Container);
-  GuiDrawPanel(CTX, &Container);
 }
 
 internal void
 PanelEnd(context* CTX) {
-  GuiPopContainer(CTX);
+  container* Panel = GuiPopContainer(CTX);
+  container* Parent = &CTX->Containers[CTX->NumContainers-1];
+  Panel->Rect.w = Panel->Content[(u32)layout::Horizontal] 
+                  + Panel->Margin[(u32)margin::Left]
+                  + Panel->Margin[(u32)margin::Right];
+  Panel->Rect.h = Panel->Content[(u32)layout::Vertical]
+                  + Panel->Margin[(u32)margin::Top]
+                  + Panel->Margin[(u32)margin::Bottom];
+  Parent->Content[(u32)layout::Horizontal] = Parent->Content[(u32)layout::Horizontal] 
+        + Panel->Content[(u32)layout::Horizontal];
+        //- (Parent->Layout == layout::Horizontal ? Parent->Gap : 0.f);
+  Parent->Content[(u32)layout::Vertical] = Parent->Content[(u32)layout::Vertical] 
+        + Panel->Content[(u32)layout::Vertical];
+        //- (Parent->Layout == layout::Vertical ? Parent->Gap : 0.f);
+  //Parent->Content[(u32)layout::Horizontal] = (Parent->Layout == layout::Horizontal
+  //      ? Parent->Content[(u32)layout::Horizontal] + Parent->Gap + Panel->Rect.w
+  //      : Panel->Content[(u32)layout::Horizontal]);
+  //Parent->Content[(u32)layout::Vertical] = (Parent->Layout == layout::Vertical
+  //      ? Panel->Content[(u32)layout::Vertical] + Parent->Gap
+  //      : Panel->Content[(u32)layout::Vertical]);
+  GuiDrawPanel(CTX, Panel);
   GuiPopId(CTX);
 }
 
 internal void
 Label(context* CTX, const char* Text) {
   u32 TextLen = (u32)strlen(Text);
-  container Parent = CTX->Containers[CTX->NumContainers-1];
-  vec2 Pos = vec2{ Parent.Rect.x + Parent.Margin[(u32)margin::Left], 
-                   Parent.Rect.y + Parent.Margin[(u32)margin::Top ] };
+  container* Parent = &CTX->Containers[CTX->NumContainers-1];
+  f32 LeftMargin = Parent->Margin[(u32)margin::Left];
+  f32 TopMargin = Parent->Margin[(u32)margin::Top];
+  f32 LeftContent = Parent->Content[(u32)layout::Horizontal];
+  f32 TopContent = Parent->Content[(u32)layout::Vertical];
+  vec2 Pos = vec2{ Parent->Rect.x + LeftMargin + LeftContent, 
+                   Parent->Rect.y + TopMargin + TopContent };
   color Color = CTX->Style.TextColor;
+  Parent->Content[(u32)layout::Vertical] += (CTX->FontData.CharHeight + Parent->Gap);
   DrawText(Text,
            TextLen,
            &CTX->FontData, 
            Pos, 
            Color, 
-           Parent.Z + 1,
+           Parent->Z + 1,
            CTX->CommandStack); 
-  
 }
 
 internal void
@@ -271,15 +340,18 @@ Button(context* CTX, const char* Text) {
   u32 TextLen = (u32)strlen(Text);
   element_id Id = GuiGetId(CTX, (const void*)Text, TextLen);
   vec2 TextSize = GetTextSize(Text, TextLen, &CTX->FontData);
+  f32 ButtonWidth = TextSize.x  + CTX->Style.ButtonMargin[(u32)margin::Left]
+                                + CTX->Style.ButtonMargin[(u32)margin::Right];
+  f32 ButtonHeight = TextSize.y + CTX->Style.ButtonMargin[(u32)margin::Top]
+                                + CTX->Style.ButtonMargin[(u32)margin::Bottom];
   container Container = CreateChildContainer(CTX, 
-                                    &CTX->Containers[CTX->NumContainers - 1]);
+                                    &CTX->Containers[CTX->NumContainers - 1],
+                                    ButtonWidth,
+                                    ButtonHeight,
+                                    layout::Horizontal);
   Container.Style = CTX->Style;
   Container.Layout = layout::Horizontal;
   memcpy(Container.Margin, CTX->Style.ButtonMargin, sizeof(Container.Margin));
-  Container.Rect.w = TextSize.x + 
-    Container.Margin[(u32)margin::Left] + Container.Margin[(u32)margin::Right];
-  Container.Rect.h = TextSize.y +
-    Container.Margin[(u32)margin::Top] + Container.Margin[(u32)margin::Bottom];
   GuiPushContainer(CTX, &Container);
   Label(CTX, Text);
   GuiPopContainer(CTX);
--- game/menu.h
+++ game/menu.h
@@ -3,17 +3,72 @@
 void DrawMenu(gui::context* CTX) {
   gui::GuiBegin(CTX);
 
-  gui::PanelBegin(CTX, "Panel");
+  CTX->Style.BaseColor = {200, 200, 200, 255};
 
-  //gui::Label(CTX, "Menu");
-  if (gui::Button(CTX, "Button")) {
+  gui::PanelBegin(CTX, "Panel", gui::layout::Vertical);
 
-  };
+    gui::Label(CTX, "Menu");
 
-  gui::Image(CTX, nullptr);
-  if (gui::ImageButton(CTX, nullptr)) {
+    gui::PanelBegin(CTX, "Panel", gui::layout::Horizontal);
 
-  };
+      CTX->Style.BaseColor = {255, 0, 0, 255};
+
+      gui::PanelBegin(CTX, "PanelInner1");
+
+        CTX->Style.BaseColor = {100, 100, 100, 255};
+        if (gui::Button(CTX, "Button 1")) {
+          printf("Button 1 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 2")) {
+          printf("Button 2 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 3")) {
+          printf("Button 3 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 4")) {
+          printf("Button 4 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 5")) {
+          printf("Button 5 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 6")) {
+          printf("Button 6 pressed\n");
+        };
+
+      gui::PanelEnd(CTX);
+    
+      CTX->Style.BaseColor = {0, 255, 0, 255};
+      
+      gui::PanelBegin(CTX, "PanelInner2");
+
+        CTX->Style.BaseColor = {100, 100, 100, 255};
+        if (gui::Button(CTX, "Button 7")) {
+          printf("Button 7 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 8")) {
+          printf("Button 8 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 9")) {
+          printf("Button 9 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 10")) {
+          printf("Button 10 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 11")) {
+          printf("Button 11 pressed\n");
+        };
+        if (gui::Button(CTX, "Button 12")) {
+          printf("Button 12 pressed\n");
+        };
+
+      gui::PanelEnd(CTX);
+
+    gui::PanelEnd(CTX);
+
+    gui::Image(CTX, nullptr);
+    if (gui::ImageButton(CTX, nullptr)) {
+
+    };
 
   gui::PanelEnd(CTX);
 
